e70c503de5e842299cfdb88cb02e62657b038c5e
EtB-1 <82047486+EternalB-1@users.noreply.github.com>
2021-04-16 18:39:08 +0300

    Update seeker.py

--- seeker.py
+++ seeker.py
@@ -58,7 +58,6 @@ def banner():
  \___ \ \  ___/\  ___/ |    < \  ___/ |  | \/
 /____  > \___  >\___  >|__|_ \ \___  >|__|
 	 \/      \/     \/      \/     \/        ''' + W)
-	print('\n' + G + '[>]' + C + ' Created By : ' + W + 'thewhiteh4t')
 	print(G + '[>]' + C + ' Version    : ' + W + version + '\n')
 
 def ver_check():
